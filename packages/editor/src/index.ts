aa01ef65aa2dc5cc1bd7c3c5c5189f2c7d6a962c
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2024-06-23 17:59:53 +0900

    add initial scene

---
+++ packages/editor/src/index.ts
@@ -0,0 +1,32 @@
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import "@babylonjs/core/Materials/standardMaterial";
+import { Scene } from "@babylonjs/core/scene";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
+
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("render-canvas") as HTMLCanvasElement;
+    const engine = new Engine(canvas);
+    const scene = new Scene(engine);
+    const camera = new UniversalCamera("EditorCamera", new Vector3(0, 5, -5), scene);
+    camera.setTarget(new Vector3(0, 1, 0));
+    camera.attachControl(true);
+
+    new DirectionalLight("LIGHT", new Vector3(0, -0.8, 0.1), scene);
+    MeshBuilder.CreateGround("GROUND", { width: 100, height: 100 }, scene);
+    const box = MeshBuilder.CreateBox("BOX", {}, scene);
+    box.position.y = 1;
+
+    const render = () => {
+        box.rotation.y += 0.01;
+        scene.render();
+    };
+    const resize = () => {
+        engine.resize();
+    };
+    engine.resize();
+    window.addEventListener("resize", resize);
+    engine.runRenderLoop(render);
+});
